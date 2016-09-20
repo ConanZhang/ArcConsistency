d023a3adc22a52b9594741d001e5359c8d499140
ConanZhang <conankzhang@gmail.com>
2016-09-19 18:20:18 -0600

    Removed parameters for driver

--- CS4300_A3_Driver.m
+++ CS4300_A3_Driver.m
@@ -1,14 +1,10 @@
-function P = CS4300_A3_Driver(i,a,j,b)
+function P = CS4300_A3_Driver()
 % CS4300_A3_Driver - Driver function for A3
 % On input:
-% i (int): start node index
-% a (int): start node domain value
-% j (int): end node index
-% b (int): end node domain value
 % On output:
 %
 % Call:
-% CS4300_A3_Driver(i,a,j,b);
+% CS4300_A3_Driver();
 % Author:
 % Rajul Ramchandani and Conan Zhang
 % UU
